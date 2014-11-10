ac43f5016fc9de87a5e44081ce8e95cfcac2ae15
ms <manfred@ms123.org>
2014-11-10 13:04:37 +0100

    cosmetic

--- bundles/camel/src/main/java/org/ms123/common/camel/CamelContextBuilder.java
+++ bundles/camel/src/main/java/org/ms123/common/camel/CamelContextBuilder.java
@@ -18,104 +18,71 @@
  */
 package org.ms123.common.camel;
 
-import flexjson.*;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.io.File;
-import java.security.MessageDigest;
-import org.ms123.common.data.api.DataLayer;
-import org.ms123.common.auth.api.AuthService;
-import org.ms123.common.utils.UtilsService;
-import org.ms123.common.git.GitService;
-import org.ms123.common.git.FileHolderApi;
-import org.ms123.common.data.api.SessionContext;
-import org.ms123.common.store.StoreDesc;
-import org.ms123.common.system.TransactionService;
-import org.ms123.common.permission.api.PermissionService;
-import org.ms123.common.datamapper.DatamapperService;
-import org.ms123.common.utils.Inflector;
-import org.ms123.common.utils.Utils;
-import org.osgi.framework.BundleContext;
-import org.apache.camel.impl.DefaultCamelContext;
-import org.apache.camel.impl.SimpleRegistry;
-import org.apache.camel.impl.CompositeRegistry;
-import org.apache.camel.impl.PropertyPlaceholderDelegateRegistry;
+import java.util.EventObject;
+import org.apache.camel.builder.DefaultErrorHandlerBuilder;
 import org.apache.camel.CamelContext;
-import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.Producer;
-import org.apache.camel.Endpoint;
-import org.apache.camel.Route;
-import org.apache.camel.Exchange;
-import org.apache.camel.spi.Registry;
 import org.apache.camel.core.osgi.OsgiDefaultCamelContext;
 import org.apache.camel.core.osgi.OsgiServiceRegistry;
-import org.apache.camel.processor.interceptor.Tracer;
-import org.apache.camel.model.ModelCamelContext;
-import org.apache.camel.builder.DefaultErrorHandlerBuilder;
-import org.apache.camel.management.event.ExchangeSentEvent;
-import org.apache.camel.management.event.ExchangeCreatedEvent;
+import org.apache.camel.Exchange;
+import org.apache.camel.impl.CompositeRegistry;
+import org.apache.camel.impl.SimpleRegistry;
 import org.apache.camel.management.event.ExchangeCompletedEvent;
-import java.util.EventObject;
+import org.apache.camel.management.event.ExchangeCreatedEvent;
+import org.apache.camel.management.event.ExchangeSentEvent;
+import org.apache.camel.model.ModelCamelContext;
+import org.apache.camel.processor.interceptor.Tracer;
+import org.apache.camel.spi.Registry;
+import org.apache.camel.spring.spi.SpringTransactionPolicy;
 import org.apache.camel.support.EventNotifierSupport;
-import org.ms123.common.libhelper.FileSystemClassLoader;
-import org.ms123.common.libhelper.ClassLoaderWrapper;
-import groovy.lang.GroovyShell;
 import org.ms123.common.camel.components.*;
 import org.ms123.common.camel.trace.*;
+import org.ms123.common.data.api.DataLayer;
+import org.ms123.common.data.api.SessionContext;
+import org.ms123.common.datamapper.DatamapperService;
+import org.ms123.common.libhelper.ClassLoaderWrapper;
+import org.ms123.common.permission.api.PermissionService;
+import org.ms123.common.system.ThreadContext;
+import org.ms123.common.system.TransactionService;
+import org.osgi.framework.BundleContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.transaction.PlatformTransactionManager;
-import org.apache.camel.spring.spi.SpringTransactionPolicy;
-import org.ms123.common.system.ThreadContext;
 
 /**
  *
  */
-@groovy.transform.CompileStatic
-@groovy.transform.TypeChecked
 public class CamelContextBuilder {
 
 	private static final Logger m_logger = LoggerFactory.getLogger(CamelContextBuilder.class);
 
-	public static synchronized ModelCamelContext createCamelContext(String namespace, Registry groovyRegistry, BundleContext bc,boolean trace) throws Exception {
+	public static synchronized ModelCamelContext createCamelContext(String namespace, Registry groovyRegistry, BundleContext bc, boolean trace) throws Exception {
 		SimpleRegistry sr = new SimpleRegistry();
 		OsgiServiceRegistry or = new OsgiServiceRegistry(bc);
-		PermissionService permissionService = (PermissionService)or.lookupByName(PermissionService.class.getName());
+		PermissionService permissionService = (PermissionService) or.lookupByName(PermissionService.class.getName());
 		sr.put(PermissionService.PERMISSION_SERVICE, permissionService);
 		sr.put(DataLayer.DATA_LAYER, or.lookupByNameAndType("dataLayer", DataLayer.class));
 		sr.put("datamapper", or.lookupByName(DatamapperService.class.getName()));
 		sr.put("namespace", namespace);
-	//	sr.put("sleepBean", new BaseCamelServiceImpl.SleepBean());
 		sr.put("activiti", new ActivitiComponent());
 		sr.put("swdata", new SWDataComponent());
-
-		TransactionService ts = (TransactionService)or.lookupByName(TransactionService.class.getName());
+		TransactionService ts = (TransactionService) or.lookupByName(TransactionService.class.getName());
 		sr.put(org.springframework.transaction.PlatformTransactionManager.class.getName(), ts.getPlatformTransactionManager());
 		createTransactionPolicies(ts.getPlatformTransactionManager(), sr);
-
 		ModelCamelContext camelContext;
 		camelContext = new OsgiDefaultCamelContext(bc);
-
 		Registry r = camelContext.getRegistry();
 		CompositeRegistry cr = new CompositeRegistry();
 		cr.addRegistry(or);
 		cr.addRegistry(sr);
 		cr.addRegistry(r);
-		if( groovyRegistry != null){
+		if (groovyRegistry != null) {
 			cr.addRegistry(groovyRegistry);
 		}
-		
 		camelContext = new OsgiDefaultCamelContext(bc, cr);
 		camelContext.setApplicationContextClassLoader(new ClassLoaderWrapper(CamelContextBuilder.class.getClassLoader(), groovy.lang.Script.class.getClassLoader()));
 		DefaultErrorHandlerBuilder dehb = new DefaultErrorHandlerBuilder();
-    dehb.logExhaustedMessageHistory(false);
-    dehb.disableRedelivery();
+		dehb.logExhaustedMessageHistory(false);
+		dehb.disableRedelivery();
 		camelContext.setErrorHandlerBuilder(dehb);
 		Tracer tracer = new Tracer();
 		tracer.setTraceOutExchanges(true);
@@ -132,71 +99,65 @@ public class CamelContextBuilder {
 		tracer.setFormatter(formatter);
 		tracer.setTraceExceptions(true);
 		tracer.setTraceHandler(new TraceEventHandler(true));
-		if( trace){
+		if (trace) {
 			camelContext.addInterceptStrategy(tracer);
 			camelContext.setTracing(true);
 		}
 		camelContext.getProperties().put(Exchange.LOG_DEBUG_BODY_STREAMS, "true");
 		camelContext.setMessageHistory(true);
-
-		camelContext.getManagementStrategy().addEventNotifier(new MyEventNotifer(namespace,permissionService));
+		camelContext.getManagementStrategy().addEventNotifier(new ExchangeEventNotifer(namespace, permissionService));
 		return camelContext;
 	}
 
-	private static void createTransactionPolicies(PlatformTransactionManager ptm, SimpleRegistry sr){
-		String[] names = { "PROPAGATION_MANDATORY",
-											 "PROPAGATION_NESTED",
-											 "PROPAGATION_NEVER",
-											 "PROPAGATION_NOT_SUPPORTED",
-											 "PROPAGATION_REQUIRED",
-											 "PROPAGATION_REQUIRES_NEW",
-											 "PROPAGATION_SUPPORTS" };
-
-		for( String name : names){
+	private static void createTransactionPolicies(PlatformTransactionManager ptm, SimpleRegistry sr) {
+		String[] names = { "PROPAGATION_MANDATORY", "PROPAGATION_NESTED", "PROPAGATION_NEVER", "PROPAGATION_NOT_SUPPORTED", "PROPAGATION_REQUIRED", "PROPAGATION_REQUIRES_NEW", "PROPAGATION_SUPPORTS" };
+		for (String name : names) {
 			SpringTransactionPolicy stp = new SpringTransactionPolicy(ptm);
 			stp.setPropagationBehaviorName(name);
-			sr.put(name, stp);	
+			sr.put(name, stp);
 		}
 	}
-	public static class MyEventNotifer extends EventNotifierSupport {
+
+	public static class ExchangeEventNotifer extends EventNotifierSupport {
 		PermissionService m_permissionService;
 		String m_namespace;
-		public MyEventNotifer(String ns, PermissionService ps){
-			m_namespace=ns;
-			m_permissionService=ps;
+
+		public ExchangeEventNotifer(String ns, PermissionService ps) {
+			m_namespace = ns;
+			m_permissionService = ps;
 		}
+
 		public void notify(EventObject event) throws Exception {
 			if (event instanceof ExchangeSentEvent) {
 				ExchangeSentEvent ev = (ExchangeSentEvent) event;
 			}
 			if (event instanceof ExchangeCreatedEvent) {
 				ExchangeCreatedEvent ev = (ExchangeCreatedEvent) event;
-				System.out.println("EventNotifierSupport:"+ev+"/"+ev.getExchange());
-					ThreadContext.getThreadContext().loadThreadContext(m_namespace,"admin");
-					m_permissionService.loginInternal(m_namespace);
+				info("EventNotifierSupport:" + ev + "/" + ev.getExchange());
+				ThreadContext.getThreadContext().loadThreadContext(m_namespace, "admin");
+				m_permissionService.loginInternal(m_namespace);
 			}
 			if (event instanceof ExchangeCompletedEvent) {
 				ExchangeCompletedEvent ev = (ExchangeCompletedEvent) event;
-				System.out.println("EventNotifierSupport:"+ev+"/"+ev.getExchange());
-					ThreadContext.getThreadContext().finalize(null);
+				info("EventNotifierSupport:" + ev + "/" + ev.getExchange());
+				ThreadContext.getThreadContext().finalize(null);
 			}
 		}
 
 		public boolean isEnabled(EventObject event) {
 			//if(event instanceof ExchangeSentEvent) return true;
-			if(event instanceof ExchangeCreatedEvent) return true;
-			if(event instanceof ExchangeCompletedEvent) return true;
+			if (event instanceof ExchangeCreatedEvent)
+				return true;
+			if (event instanceof ExchangeCompletedEvent)
+				return true;
 			return false;
 		}
 
 		protected void doStart() throws Exception {
-			// noop
 		}
 
 		protected void doStop() throws Exception {
-			// noop
 		}
-
 	}
 
 	private static void info(String msg) {
