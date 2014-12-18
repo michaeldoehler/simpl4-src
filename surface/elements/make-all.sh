7293a2d7009a4b299c10cdd76a852acd4e92e485
ms <manfred@ms123.org>
2014-12-18 03:31:33 +0100

    intermediate commit

--- surface/elements/make-all.sh
+++ surface/elements/make-all.sh
@@ -2,13 +2,13 @@
 
 bower install
 
-rm -f index.html
-ALL=`fgrep -l polymer-element */*/*.html`
+#rm -f index.html
+#ALL=`fgrep -l polymer-element */*/*.html|grep -v demo.html`
 
-for i in $ALL
-do
-  echo '<link rel="import" href="'$i'">' >>index.html
-done
+#for i in $ALL
+#do
+#  echo '<link rel="import" href="'$i'">' >>index.html
+#done
 
 vulcanize --inline index.html
 mv vulcanized.html elements.html
