3a4c160078bf7c67be35b14c3c58151676caaf42
ms <manfred@ms123.org>
2015-02-01 07:26:30 +0100

    take the id from encapsulated-element as shimSelector

--- surface/js/libs/webcomponents.js
+++ surface/js/libs/webcomponents.js
@@ -4231,6 +4231,10 @@ if (WebComponents.flags.shadow) {
         var scopeStyles = this.prepareRoot(root, name, extendsName);
         var typeExtension = this.isTypeExtension(extendsName);
         var scopeSelector = this.makeScopeSelector(name, typeExtension);
+				if( name.match(/encapsulate.*/)){
+					console.log("Treffer");
+					scopeSelector = "#"+scopeSelector;
+				}    
         var cssText = stylesToCssText(scopeStyles, true);
         cssText = this.scopeCssText(cssText, scopeSelector);
         if (root) {
