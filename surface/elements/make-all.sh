554b3e1ec858fde3c5aed5e3999fef613d17d9d0
ms <manfred@ms123.org>
2015-02-01 08:15:33 +0100

    intermediate save

--- surface/elements/make-all.sh
+++ surface/elements/make-all.sh
@@ -5,5 +5,5 @@ rm -rf bower_components/polymer/
 find . -name "*.html" -exec sed -i "/polymer.html/d" {} \;
 
 
-vulcanize --inline index.html
+vulcanize --config vulcan.config --inline index.html
 mv vulcanized.html elements.html
