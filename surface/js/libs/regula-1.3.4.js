12223617f651a2121871b027eb81cdc67196b2f5
ms <manfred@ms123.org>
2015-02-09 21:50:47 +0100

    setting validateEmptyField false,you can do this with NotBlank, NotEmpty

--- surface/js/libs/regula-1.3.4.js
+++ surface/js/libs/regula-1.3.4.js
@@ -5219,7 +5219,7 @@
 	 * @type {{validateEmptyFields: boolean, enableHTML5Validation: boolean, debug: boolean}}
 	 */
 	var config = {
-		validateEmptyFields: true,
+		validateEmptyFields: false,
 		enableHTML5Validation: false,
 		debug: false
 	};
