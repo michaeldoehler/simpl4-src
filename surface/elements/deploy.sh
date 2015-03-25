6a4016d772e4826e1815463d509a12bfcf07d321
ms <manfred@ms123.org>
2015-03-25 05:10:21 +0100

    handle hash fragments with an exclamation mark

--- surface/elements/deploy.sh
+++ surface/elements/deploy.sh
@@ -2,4 +2,4 @@
 make-all.sh
 gzip -f elements.html
 scp elements.html.gz  ms:$simpl4first/client/surface/
-scp elements.html.gz  ms:$fis/client/surface/
+scp elements.html.gz  ms:$web/client/surface/
