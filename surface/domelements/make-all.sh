3e3b8fc4f7fb25981364d7ed8c36160f9dcd5b4f
ms <manfred@ms123.org>
2015-07-09 14:18:20 +0200

    intermediate save
    intermediate save
    
    intermediate save
    
    intermediate save

--- surface/domelements/make-all.sh
+++ surface/domelements/make-all.sh
@@ -4,5 +4,6 @@
 #bower install
 
 #sed -i '/slide-up-scale-down-animation.html/d'  bower_components/neon-animation/neon-animations.html
+#sed -i 's!value: Polymer.IronOverlayManager!value: function(){ return Polymer.IronOverlayManager}!' bower_components/iron-overlay-behavior/*.html
 
 /usr/bin/vulcanize  --strip-comments --inline-css --inline-scripts --inline index.html >domelements.html
