545b1c223fba6bb9671bb9e600216a1663700df4
ms <manfred@ms123.org>
2015-07-14 03:13:07 +0200

    intermediate save,multiline xaction-button

--- surface/domelements/make-all.sh
+++ surface/domelements/make-all.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-#rm -rf bower_components  
-#bower install
-
-#sed -i '/slide-up-scale-down-animation.html/d'  bower_components/neon-animation/neon-animations.html
-#sed -i 's!value: Polymer.IronOverlayManager!value: function(){ return Polymer.IronOverlayManager}!' bower_components/iron-overlay-behavior/*.html
+#rm -rf bower_components
+if [ ! -d "bower_components" ] ; then
+   echo "No bower"
+   bower install
+   
+   sed -i '/slide-up-scale-down-animation.html/d'  bower_components/neon-animation/neon-animations.html
+   sed -i 's!value: Polymer.IronOverlayManager!value: function(){ return Polymer.IronOverlayManager}!' bower_components/iron-overlay-behavior/*.html
+fi
 
 /usr/bin/vulcanize  --strip-comments --inline-css --inline-scripts --inline index.html >domelements.html
