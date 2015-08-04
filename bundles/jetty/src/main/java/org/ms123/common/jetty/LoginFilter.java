c610f6499063b7a463465e91b72990febf3f2ad3
ms <manfred@ms123.org>
2015-08-04 18:24:53 +0200

    repo access, with access control

--- bundles/jetty/src/main/java/org/ms123/common/jetty/LoginFilter.java
+++ bundles/jetty/src/main/java/org/ms123/common/jetty/LoginFilter.java
@@ -22,6 +22,9 @@ import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.util.Date;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
 import java.util.Enumeration;
 import java.sql.Timestamp;
 import com.Ostermiller.util.*;
@@ -29,10 +32,18 @@ import org.ms123.common.permission.api.PermissionService;
 import org.ms123.common.system.thread.ThreadContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import flexjson.*;
+import org.springframework.util.AntPathMatcher;
+import static org.apache.commons.io.FileUtils.readFileToString;
 
 public class LoginFilter implements Filter {
 
+
+	private String PERMITTED_USERS = "permittedUsers";
+	private String PERMITTED_ROLES = "permittedRoles";
+	private String USERNAME = "username";
 	private FilterConfig config = null;
+	private JSONDeserializer ds = new JSONDeserializer();
 
 	private PermissionService m_permissionService;
 
@@ -72,7 +83,11 @@ public class LoginFilter implements Filter {
 		if (arr.length < 2) {
 			namespace = "RPC";
 		} else {
-			namespace = arr[1];
+			if (arr[1].equals("repo")) {
+				namespace = arr[2];
+			} else {
+				namespace = arr[1];
+			}
 		}
 		for (int i = 0; i < arr.length; i++) {
 			debug("\tpathinfo[" + i + "]:" + arr[i]);
@@ -102,67 +117,65 @@ public class LoginFilter implements Filter {
 			int ind = credentials.indexOf(":");
 			if (ind != -1) {
 				String username = credentials.substring(0, ind);
-				req.setAttribute("username", username);
+				req.setAttribute(USERNAME, username);
 			}
 		}
-		boolean ok = 
-				(arr.length>3 && "sw".equals(arr[1]) && "website".equals(arr[3])) ||
-				pathInfo.startsWith("/" + namespace + "/common") || 
-				pathInfo.startsWith("/robots.txt") || 
-				pathInfo.startsWith("/" + namespace + "/website") || 
-				(pathInfo.startsWith("/" + namespace + "/") && pathInfo.endsWith(".html")) ||
-				(pathInfo.startsWith("/" + namespace + "/") && pathInfo.endsWith(".html.gz")) ||
-				(pathInfo.startsWith("/" + namespace + "/") && pathInfo.endsWith(".js")) ||
-				(pathInfo.startsWith("/" + namespace + "/") && pathInfo.endsWith(".js.gz")) ||
-				(pathInfo.startsWith("/" + namespace + "/") && pathInfo.endsWith(".css")) ||
-				(pathInfo.startsWith("/" + namespace + "/") && pathInfo.endsWith(".css.gz")) ||
-				(pathInfo.startsWith("/" + namespace + "/") && pathInfo.endsWith(".adoc")) ||
-				(pathInfo.startsWith("/" + namespace + "/") && pathInfo.endsWith(".adoc.gz")) ||
-				(pathInfo.startsWith("/" + namespace + "/") && pathInfo.endsWith(".json")) ||
-				(pathInfo.startsWith("/" + namespace + "/") && pathInfo.endsWith(".json.gz")) ||
-				(pathInfo.startsWith("/" + namespace + "/") && pathInfo.endsWith(".yaml")) ||
-				(pathInfo.startsWith("/" + namespace + "/") && pathInfo.endsWith(".yml")) ||
-				(pathInfo.startsWith("/" + namespace + "/") && pathInfo.endsWith(".png")) ||
-				(pathInfo.startsWith("/" + namespace + "/") && pathInfo.endsWith(".jpg")) ||
-				(pathInfo.startsWith("/" + namespace + "/") && pathInfo.endsWith(".gif")) ||
-				(pathInfo.startsWith("/" + namespace + "/") && pathInfo.endsWith(".odt")) ||
-				(pathInfo.startsWith("/" + namespace + "/") && pathInfo.endsWith(".svg")) ||
-				(pathInfo.startsWith("/" + namespace + "/") && pathInfo.endsWith(".xml")) ||
-				(pathInfo.startsWith("/" + namespace + "/") && pathInfo.endsWith(".jpeg")) ||
-				(pathInfo.startsWith("/" + namespace + "/") && pathInfo.endsWith(".txt")) ||
-				(pathInfo.startsWith("/" + namespace + "/") && pathInfo.endsWith(".woff")) ||
-				(pathInfo.startsWith("/" + namespace + "/") && pathInfo.endsWith(".woff.gz")) ||
-				pathInfo.startsWith("/" + namespace + "/dav") || 
-				pathInfo.startsWith("/" + namespace + "/legacy") || 
-				pathInfo.startsWith("/openfire") || 
-				pathInfo.startsWith("/" + namespace + "/resource");
-		if (!ok) {
-			String origin = request.getHeader("Origin");
-			String method = request.getMethod();
-			debug("Origin:" + origin + "," + method+"|pi:"+pathInfo);
-			if (origin != null && method != null && "options".equals(method.toLowerCase())) {
+		String firstSegment = arr[1];
+		String method = request.getMethod();
+		boolean isStaticResource = false;
+		boolean ok = false;
+		if (method != null && "get".equals(method.toLowerCase())) {
+			if (arr.length > 3 && "sw".equals(arr[1]) && "website".equals(arr[3])) {
+				ok = true;
+			} else if ("sw".equals(firstSegment) || "repo".equals(firstSegment)) {
+				if (isStaticResource(pathInfo)) {
+					ok = true;
+					isStaticResource = true;
+				}
+			} else if (pathInfo.equals("/robots.txt")) {
+				ok = true;
+			} else if (pathInfo.startsWith("/openfire")) {
+				ok = true;
+			} else if (pathInfo.startsWith("/sw/common") || pathInfo.startsWith("/sw/website") || pathInfo.startsWith("/sw/legacy") || pathInfo.startsWith("/sw/resource")) {
 				ok = true;
 			}
+		} else if (request.getHeader("Origin") != null && method != null && "options".equals(method.toLowerCase())) {
+			ok = true;
 		}
 
-		if( request.getParameter("rpc")!= null || request.getRequestURI().startsWith("/rpc")){
-			ok=false;
+		if (request.getParameter("rpc") != null || request.getRequestURI().startsWith("/rpc")) {
+			ok = false;
 		}
-		if( request.getParameter("ws")!= null || request.getRequestURI().startsWith("/ws")){
-			ok=false;
+		if (request.getParameter("ws") != null || request.getRequestURI().startsWith("/ws")) {
+			ok = false;
 		}
-		
 
+		Map<String, Object> accessRule = null;
+		if (isStaticResource && isRepoRequest(pathInfo)) {
+			List<Map<String, Object>> rules = getAccessRules(namespace);
+			accessRule = getMatchingAccessRule(namespace, pathInfo, rules);
+			if (accessRule != null) {
+				ok = false;
+			}
+		}
 
-		info(pathInfo + ";" + credentials+"/ok:"+ok);
+		info(pathInfo + ";" + credentials + "/ok:" + ok + "/accessRule:"+accessRule);
 		if (ok || checkCredentials(namespace, credentials, false)) {
-			info(">>>> OK,"+Thread.currentThread().getName());
+			if (accessRule != null) {
+				String username = (String) req.getAttribute(USERNAME);
+				if (!isPermitted(username, getUserRoles(username), (List) accessRule.get(PERMITTED_USERS), (List) accessRule.get(PERMITTED_ROLES))) {
+					response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+					return;
+				}
+			}
+			info(">>>> OK," + Thread.currentThread().getName());
 			RequestMapper rm = new RequestMapper(request);
 			ThreadContext.loadThreadContext(rm, response);
-			info(request.getPathInfo() + "|" + request.getMethod()+"|Uri:"+request.getRequestURI());
-			info(request.getRequestURL() + "|Url:" + request.getServletPath()+"|QS:"+request.getQueryString());
+			info(request.getPathInfo() + "|" + request.getMethod() + "|Uri:" + request.getRequestURI());
+			info(request.getRequestURL() + "|Url:" + request.getServletPath() + "|QS:" + request.getQueryString());
 			chain.doFilter(rm, response);
-			info(">>>> End FILTER:"+ThreadContext.getThreadContext().get(ThreadContext.SESSION_MANAGER)+"/"+new Date().getTime());
+			info(">>>> End FILTER:" + ThreadContext.getThreadContext().get(ThreadContext.SESSION_MANAGER) + "/"
+					+ new Date().getTime());
 			Date startTime = ThreadContext.getThreadContext().getStartTime();
 			ThreadContext.getThreadContext().finalize(null);
 			ThreadContext.getThreadContext().remove();
@@ -194,6 +207,66 @@ public class LoginFilter implements Filter {
 		}
 	}
 
+	private List<String> getUserRoles(String userName) {
+		List<String> userRoleList = null;
+		try {
+			userRoleList = m_permissionService.getUserRoles(userName);
+		} catch (Exception e) {
+			userRoleList = new ArrayList();
+		}
+		return userRoleList;
+	}
+
+	private boolean isPermitted(String userName, List<String> userRoleList, List<String> permittedUserList, List<String> permittedRoleList) {
+		info("UserRoleList:" + userRoleList);
+		info("PermittedUserList:" + permittedUserList);
+		if (permittedUserList != null && permittedUserList.contains(userName)) {
+			info("userName(" + userName + ") is allowed:" + permittedUserList);
+			return true;
+		}
+		info("permittedRoleList:" + permittedRoleList);
+		for (String userRole : userRoleList) {
+			if (permittedRoleList != null && permittedRoleList.contains(userRole)) {
+				info("userRole(" + userRole + ") is allowed:" + permittedRoleList);
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private boolean isRepoRequest(String pathInfo) {
+		return pathInfo.startsWith("/repo");
+	}
+
+	private List<Map<String, Object>> getAccessRules(String namespace) {
+		File file = new File(System.getProperty("git.repos") + "/" + namespace + "/.etc/access-rules.json");
+		String content = null;
+		try {
+			content = readFileToString(file);
+		} catch (Exception e) {
+			info("LoginFilter.getAccessRules:" + e.getMessage());
+			return null;
+		}
+		return (List) this.ds.deserialize(content);
+	}
+
+	private Map<String, Object> getMatchingAccessRule(String ns, String pathInfo, List<Map<String, Object>> rules) {
+		if (rules == null) {
+			return null;
+		}
+		String resPath = pathInfo.substring(("/repo/" + ns + "/").length());
+		AntPathMatcher a = new AntPathMatcher();
+		for (Map<String, Object> rule : rules) {
+			String pat = (String) rule.get("pattern");
+			boolean res = a.match(pat, resPath);
+			info("\tmatch:" + pat + " -> " + res);
+			if (res) {
+				return rule;
+			}
+		}
+		return null;
+	}
+
 	private boolean noAuth() {
 		String sh = System.getProperty("workspace");
 		try {
@@ -204,24 +277,42 @@ public class LoginFilter implements Filter {
 		}
 		return false;
 	}
-	 public void displayInfo( String where, Date startTime ) {
-    long time = new Date().getTime() - startTime.getTime();
-    long fm = Runtime.getRuntime().freeMemory() / ( 1024 * 1024 );
-    long tm = Runtime.getRuntime().totalMemory() / ( 1024 * 1024 );
-    info( "Memory(" + where + "):free=" + fm + "M,total=" + tm + "M,time:" + time +" mSec");
-  }
 
+	private boolean isStaticResource(String pathInfo) {
+		if (pathInfo.endsWith(".html") || pathInfo.endsWith(".html.gz") || pathInfo.endsWith(".js")
+				|| pathInfo.endsWith(".js.gz") || pathInfo.endsWith(".css") || pathInfo.endsWith(".css.gz")
+				|| pathInfo.endsWith(".adoc") || pathInfo.endsWith(".adoc.gz") || pathInfo.endsWith(".json")
+				|| pathInfo.endsWith(".json.gz") || pathInfo.endsWith(".yaml") || pathInfo.endsWith(".yml")
+				|| pathInfo.endsWith(".png") || pathInfo.endsWith(".jpg") || pathInfo.endsWith(".gif")
+				|| pathInfo.endsWith(".odt") || pathInfo.endsWith(".svg") || pathInfo.endsWith(".xml")
+				|| pathInfo.endsWith(".jpeg") || pathInfo.endsWith(".txt") || pathInfo.endsWith(".woff")
+				|| pathInfo.endsWith(".woff.gz")) {
+			return true;
+		}
+		return false;
+	}
+
+	public void displayInfo(String where, Date startTime) {
+		long time = new Date().getTime() - startTime.getTime();
+		long fm = Runtime.getRuntime().freeMemory() / (1024 * 1024);
+		long tm = Runtime.getRuntime().totalMemory() / (1024 * 1024);
+		info("Memory(" + where + "):free=" + fm + "M,total=" + tm + "M,time:" + time + " mSec");
+	}
 
 	protected void debug(String msg) {
 		m_logger.debug(msg);
 	}
+
 	private void infob(String msg) {
 		System.out.println(msg);
 		m_logger.info(msg);
 	}
+
 	protected void info(String msg) {
 		System.out.println("LoginFilter:" + msg);
 		m_logger.info(msg);
 	}
+
 	private static final org.slf4j.Logger m_logger = org.slf4j.LoggerFactory.getLogger(LoginFilter.class);
 }
+
