c906912d63efee5c8aa024a546cc1ebf2d12409b
ms <manfred@ms123.org>
2015-08-16 04:03:29 +0200

    ie9  polymer.* patches

--- surface/domelements/make-all.sh
+++ surface/domelements/make-all.sh
@@ -8,6 +8,11 @@ if [ ! -d "bower_components" ] ; then
    sed -i '/slide-up-scale-down-animation.html/d'  bower_components/neon-animation/neon-animations.html
    sed -i 's!value: Polymer.IronOverlayManager!value: function(){ return Polymer.IronOverlayManager}!' bower_components/iron-overlay-behavior/*.html
    sed -i '/    html {/,+14d'  bower_components/mat-typography/mat-typography.html
+	 sed -e '/target.insertBefore/ {' -e 'r polymer.patch' -e 'd' -e '}' -i bower_components/polymer/polymer.html
+
+
+	sed -i 's/console.\(log\|warn\|error\).apply/Function.prototype/' bower_components/polymer/polymer-micro.html
+	sed -i '/function saveLightChildrenIfNeeded/a if( node == null){ console.log("saveLightChildrenIfNeeded:node is null"); return; }' bower_components/polymer/polymer-mini.html
 fi
 
 /usr/bin/vulcanize  --strip-comments --inline-css --inline-scripts --inline index.html >domelements.html
