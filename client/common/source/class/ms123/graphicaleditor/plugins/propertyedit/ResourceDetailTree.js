26d194187220e5d39e2b4d7ea43d573f8b76e520
ms <manfred@ms123.org>
2015-09-24 09:53:47 +0200

    resourcehelper fot camel

--- client/common/source/class/ms123/graphicaleditor/plugins/propertyedit/ResourceDetailTree.js
+++ client/common/source/class/ms123/graphicaleditor/plugins/propertyedit/ResourceDetailTree.js
@@ -84,7 +84,7 @@ qx.Class.define("ms123.graphicaleditor.plugins.propertyedit.ResourceDetailTree",
 		},
 		_getResource: function (includeList, excludeList) {
 			var t = ms123.util.Remote.rpcSync("git:getWorkingTree", {
-				name: this.__storeDesc.getNamespace(),
+				name: this._getNamespace(),
 				includeTypeList: includeList,
 				excludePathList: excludeList,
 				mapping: {
@@ -219,7 +219,7 @@ qx.Class.define("ms123.graphicaleditor.plugins.propertyedit.ResourceDetailTree",
 						var resource = childs.getItem(0).getResource();
 						var type = childs.getItem(0).getType();
 						formDesc = ms123.util.Remote.rpcSync("git:searchContent", {
-							reponame: this.__storeDesc.getNamespace(),
+							reponame: this._getNamespace(),
 							name: resource,
 							type: type
 						});
@@ -235,22 +235,6 @@ qx.Class.define("ms123.graphicaleditor.plugins.propertyedit.ResourceDetailTree",
 						return;
 					}
 					this._getFormFields(formDesc, fieldList);
-			/*	}else if( item.getType() == "sw.document"){
-					var formDesc = null;
-					try {
-						var resource = childs.getItem(0).getResource();
-						var type = childs.getItem(0).getType();
-						formDesc = ms123.util.Remote.rpcSync("git:searchContent", {
-							reponame: this.__storeDesc.getNamespace(),
-							name: resource,
-							type: type
-						});
-						formDesc = formDesc.evalJSON();
-					} catch (e) {
-						ms123.form.Dialog.alert("ResourceDetailTree._onOpenNode:" + e);
-						return;
-					}
-					this._getFormFields(formDesc, fieldList);*/
 				}else if( item.getType() == "sw.entitytype"){
 					var fields = null;
 					try {
@@ -271,7 +255,7 @@ qx.Class.define("ms123.graphicaleditor.plugins.propertyedit.ResourceDetailTree",
 						var resource = childs.getItem(0).getResource();
 						var type = childs.getItem(0).getType();
 						enumeration = ms123.util.Remote.rpcSync("git:searchContent", {
-							reponame: this.__storeDesc.getNamespace(),
+							reponame: this._getNamespace(),
 							name: resource,
 							type: type
 						});
@@ -288,7 +272,7 @@ qx.Class.define("ms123.graphicaleditor.plugins.propertyedit.ResourceDetailTree",
 						var resource = childs.getItem(0).getResource();
 						var type = childs.getItem(0).getType();
 						filter = ms123.util.Remote.rpcSync("git:searchContent", {
-							reponame: this.__storeDesc.getNamespace(),
+							reponame: this._getNamespace(),
 							name: resource,
 							type: type
 						});
@@ -315,6 +299,20 @@ qx.Class.define("ms123.graphicaleditor.plugins.propertyedit.ResourceDetailTree",
 					}else{
 						this._getFilterFields(filter, fieldList);
 					}
+				}else if( item.getType() == "sw.camel"){
+					var procedureShapes = null;
+					try {
+						var resource = childs.getItem(0).getResource();
+						var type = childs.getItem(0).getType();
+						procedureShapes = ms123.util.Remote.rpcSync("camel:getProcedureShapesForPrefix", {
+							prefix: this._getNamespace() + "/" + item.getValue()
+						});
+					} catch (e) {
+						ms123.form.Dialog.alert("ResourceDetailTree._onOpenNode:" + e);
+						childs.removeAll();
+						return;
+					}
+					fieldArray= this._getCamelFields(procedureShapes,item);
 				}
 				if( fieldArray == null){
 					fieldArray = this._createChilds(fieldList, item.getValue());
@@ -324,15 +322,21 @@ qx.Class.define("ms123.graphicaleditor.plugins.propertyedit.ResourceDetailTree",
 				childs.append(model);
 			}
 		},
-		_createChilds:function(fieldList, _parent){
+		_createChilds:function(fieldList, _parent, type){
 			var fieldarray = [];
 			for (var i = 0; i < fieldList.length; i++) {
 				var fname = fieldList[i].name;
-				var id = fieldList[i].id;
+				var id = fieldList[i].id || fieldList[i].name;
 				var f = {}
-				f.type = "sw.field";
+				if( type ){
+					f.type = type + "_" + fieldList[i].type;
+				}else{
+					f.type = "sw.field";
+				}
 				f.value = fname;
-				f.parent = _parent;
+				if( _parent){
+					f.parent = _parent;
+				}
 				f.id = id || fname;
 				f.title = fname;
 				if( fieldList[i].columns){
@@ -372,6 +376,23 @@ qx.Class.define("ms123.graphicaleditor.plugins.propertyedit.ResourceDetailTree",
 				colarray.push(c);
 			}
 			return colarray;
+		},
+		_getCamelFields: function (procedureShapes,item) {
+			var fieldArray=[];
+			if( procedureShapes.length==0){
+				return null;
+			}
+			for( var i=0; i < procedureShapes.length;i++){
+				var procedureShape = procedureShapes[i];
+				var stencilId = procedureShape.stencil.id.toLowerCase();
+				var properties = procedureShape.properties;
+				var rpcParameter = properties.rpcParameter ? properties.rpcParameter.items : [];
+				var dir = this._createDirectory(properties.urivalue_name,item.getValue());
+				dir.children = this._createChilds(rpcParameter,null,"camelparam");
+				fieldArray.push(dir);
+			}
+			console.log("Push:", JSON.stringify(fieldArray,null,2));
+			return fieldArray;
 		},
 		_getFormFields: function (formDesc, fieldList) {
 			var stencilId = formDesc.stencil.id.toLowerCase();
@@ -481,6 +502,9 @@ qx.Class.define("ms123.graphicaleditor.plugins.propertyedit.ResourceDetailTree",
 				return this._getIconUrl("forms/new_input.png");
 			}else if (type == "sw.directory") {
 				return "resource/ms123/directory.png";
+			}else if (type.startsWith("camelparam")) {
+				var x = type.split("_");
+				return this.getDataTypeIcon('_'+x[1]);
 			}else if (type == "sw.field") {
 				var id = model.getId ? model.getId().toLowerCase() : "";
 				if( id == "enumselect" ){
@@ -504,9 +528,26 @@ qx.Class.define("ms123.graphicaleditor.plugins.propertyedit.ResourceDetailTree",
 			}
 			return "resource/ms123/file.png";
 		},
+		getDataTypeIcon:function(type){
+			var map = {
+				_string: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAMAAAAoLQ9TAAAAtFBMVEUAAAD29/z4+fz19/z09vvx9Pvo7vrr8Pru8vrx9Pr5+vzj6/nt8vvw9Pvh6vnj6/jo7/rq8Prt8vrz9vvg6vni6/nh6vjj7Pnl7fno7/nr8frw9Pr3+fzg6vjj7Pjn7/rr8fnx9fopSGuQrs6iw+drgZnt8/r1+Pt9dU57dE+FeUyCd02kiUGfhkOahEWVgUeQfkiLe0upi0DgyI+0kD2xjj6ujT+qikDUsmjUsmnavXzVsml/JLp3AAAAAXRSTlMAQObYZgAAAAFiS0dEAIgFHUgAAAAJcEhZcwAACxMAAAsTAQCanBgAAAAHdElNRQffCRgGMxEtVyArAAAAlUlEQVQY023PxxaCMBBGYewFLJEgscQycQTpqKDi+7+XyqBuuMtvMf8ZTavpWvWDbFj2yL6QExTPvILbuqkA4FLczwRBowV4QHCcgCBsm4Dmp5Ag6owQAPerQUQQG0ZfIeBxFhMkjPXYEoHtEoJ0uyiPdqcpgSeEeM8qITwC39It257o+sYncCWXkvO5HLsEp381r78AhOcSEfdXGPIAAAAASUVORK5CYII=',
+				_map :'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAMAAAAoLQ9TAAAAxlBMVEUAAABolMV0ncmCp9CJrNOLrtTX4/He6PPk7PVFfbRNg7hLf7NOg7hNgLRRhrtOgbVShrpTh7tThrldjr9gkcJfj8BiksNhkcJkk8NmlMRmlMNum8humsd2ocx7pM5+ps59pc2CqM+IrNKRs9aTtNeWtteXtteaudqowdupwturxN2xyOC80ebc5vDl7fVOgrRPg7VSiLpVirxVibtWirxXi7tbjr5dj75ilMNfkL5hksCqw9uwyN+yyuDf6fKmxN7H2ur///9YyPm8AAAAAXRSTlMAQObYZgAAAAFiS0dEAIgFHUgAAAAJcEhZcwAACxMAAAsTAQCanBgAAAAHdElNRQfdCgMSBA/RAL2VAAAApElEQVQY042P2xaBUBRFDyVFCoUuinRSqBRyCdX+/58ydm4vHsy3NccaY+1NyE8qdqxO2OqTPdVkGMZUvXfOHL8sS9/JnoaT7NhG4pnEoWCnIfCWZfEQNl0U2nEDUIwKgNtJQzHoBSBHEMkQdPt1I11BkkRJAsG2briHNQwRuO6XKNpCXuTI/SzUK4R2GnPksqOvy6ioG4ahi/T7y0JJlVZF/uIBSGAUYfqEPuUAAAAASUVORK5CYII=',
+				_list: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAMAAAAoLQ9TAAAAPFBMVEUAAAAAgIBfn5+fv784mFg4eDg4WDh/n3/A2MBffz9/n1+YuHjY2Jjf37/4+PD///+fn5+AgIBfX19YWFheY3MBAAAAAXRSTlMAQObYZgAAAAFiS0dEAIgFHUgAAAAJcEhZcwAACxMAAAsTAQCanBgAAAAHdElNRQfdCgMSBxaeRkaWAAAAbklEQVQY05WO0RKDMAgEEy0ngvTU/v+/lljTTB+7b9zsAaUUl8TLF7Egw6TPbnTAad2RIJ6OLboipOIF5Qg2ZCmD/WxkReEZxMrG2ZY+ADNd5lqnDO6zCt7G9RjWhSl8jIudHEbj4FR/jON/Y/AG2/AHPeWumhMAAAAASUVORK5CYII=',
+				_integer: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAMAAAAoLQ9TAAAAzFBMVEUAAADFvODFvd7X0elwXrBvXa5yYLFxX7BzYbF1ZLF4ZrR9bLh9bLeCcrmPgMKXiMaVh8WYiseekcucj8mfksuilsylmc+onNCupNGxp9Tc1+zi3vBVQp9aR59dSqRaSJ9ZR55cSaBeS6RdSqFcSqBdS6FhT6dgT6ZhT6ZiUKdkUqhiUKVgT6JkUqdlU6hlVKlsW61rW6xsXKxxYLFvXq51ZLKDdLyJer+GeLuLfb+KfL6OgMGhlcyupdK5sNrMxuReTqFpWayupdT///9zohUCAAAAAXRSTlMAQObYZgAAAAFiS0dEAIgFHUgAAAAJcEhZcwAACxMAAAsTAQCanBgAAAAHdElNRQffCRgHBhzwFfQXAAAAqElEQVQY05WPXReBQBRFQ0OJQikSjUI+okIjitL9//9JE/Fsv929zlrnXIYpSKUm25JSpiJhBVEUBTapbi9W8zxXY+9t9NVFg4KGdl3pVEgdQgYANUJIV6aCeyKEAB4IBQFHxWZuYwwwwtiersuEszRNgLuJ7UWZkA87NwMYZ667H1IxMc4RbalHJ6NsYZRZ+0Y5bpXPMsUKeZ4PLeW7Pe35jt///fInL3ghFal4aGkqAAAAAElFTkSuQmCC',
+				_boolean: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAMAAAAoLQ9TAAABXFBMVEUAAAAGDAULEgoNEwsNFwkPFw0QFw4OGQoSFhEQHQwTGw8SHA8WGRUQHgwSIQ0ZHRcTIw4cIBsZLxMmRhwmRxwnSR08QTstUiEwWSQxWiQxWyRGS0RHS0UyXCQzXSVKUEg1YSdMUUo2ZCg5aCk5aSpVW1M8byw8cCw+cS0/dC5Adi9BeDBhYWFiYmJDfDFjY2NgZl9kZGREfjJFfzNGgjNGgjRHgzRlbGNJhjVKiDZMizdNjThQkzpSlzxVnD5YoUBYo0GDh4KGioWIjIdfrkWIjYeJjoiMkItitUiOk41jtkmQlo9lukplu0pmvEtnvUtnvktnvkxov0xowExpwk1qwk1qw01qw05qxE5rxE5rxk5rxk9sxk9sx09syE9tyE+0vbK8xLrHzMXIzsbHz8XKz8jP1M3O1czR1s/R1tDU2dLV29LY3dba4Njf5N7k6OPk6ePo6+f9/v3///9HAKs+AAAA5ElEQVQY02MoBoFMT0N99zQwkwGIcxT5JE3NZAVksiACiWzagdGhIdFBeqwJIIFcZrsYXx8g8I22Z8oGCkhrxfrIMTAwsFj5xBiJFTFk8Ab4+sjJ+fhYMfr4BAskM7hJRPmABVyAAtEKjgw6JmFAARYhIUZlH58QSx0GfeNwoICwlZWmAFDAQpfBWzwaosVHyMonWsqVIYfdF2KoLaODrz9nBkOxqko8xFpNn1gNeaA7Cnis4/1ADvOLs+HIAzk9XVDJIyoyItpLmT8F4rlCcy4RNXVRboN8qG+Li1Od9fWdksBMAIWiRLLlwAe2AAAAAElFTkSuQmCC',
+				_double: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABmJLR0QA/wD/AP+gvaeTAAAAB3RJTUUH2QcRExQLBHwYWQAAAm9JREFUOI2Fk0tIlFEUx3/fNz7HwdGpmTRNhRmLJDVJsUUPI1+LQvLVorDaqEnCWOHaoqBdJZhItCho45j5iMgHhNODghYqKqUjMaaj46hkjlIz+t0W1uCk2H93zzm//73n3Hsl/tXL6mBWvAUIkY8gAVAhMQH0IFStlD5wbyyX/GBL5SkQDSnamLiS2EOYNAYCJJnxZRcdjkHez487gVpKmp5s2hhLuVnbZl5r/vZJKEIRW6lrZlhEd9YKLJU3/eGWyjxtm3l14PvkluBG2Zfn101ayi+st9BUHohOGn6ccTGxLP4wAOaBZt7O2QiQVEQEhpKui+fcnkz2h0cB0O0cIe9N/Sy/ghJldCLbpNEnno/L9B3I5nYxtOggS7+XmNAIGmx9pPXewjo3BkDuriSO7DQZCPYUySDlnI5OQZb85xmiCuRO8hkepZdhzbqGAG6MvPDlC3anAiJXRhBn1Og3D3WDkrUxpGpjGVp0+GLGMD0gx8lIklCE2NYAYGn1J+qAIN9aIAChyEh8HXPPbgu/dn1h1O3khH6fLza65ASwy0hKd4djgFWh+EErax5KPzwk23qPHOt9TBoDtw8U+PKtU/0g8UpFsdG+6KUwKiTckKFLAGBiZQFtoJoQVQAmjYEq43HqD54lMkgNwPOpfu6O9U4i1FXrdpbLR9Wt1Z53c7b/PqTPP2aErv2qQktlkX+jlopLoa1XvI3jfcKrrG0CFaGIp/aPIrK9RsFScf0v5n/5zyqOodCYELYjqTAmjcQ/n8nmdtE5PcjI0vQ4UENxU+fWBgB1dTJJ0yeRyUdgBCGBZEeiB2Whi1KLZ2P5bwjMSQjLsL1pAAAAAElFTkSuQmCC'
+			}
+			return map[type];
+		},
 		_getIconUrl: function (name) {
 			var am = qx.util.AliasManager.getInstance(name);
 			return am.resolve("resource/ms123/stencilsets/" + name);
+		},
+		_getNamespace:function(){
+			if(this.config.namespace && this.config.namespace!='-'){
+				return this.config.namespace;
+			}
+			return this.__storeDesc.getNamespace();
 		},
 		_toArray: function (map) {
 			var arr = [];
