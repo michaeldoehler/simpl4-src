e3dbde6d5409c0c86490502b06357d737583d231
ms <manfred@ms123.org>
2015-10-27 21:49:29 +0100

    remove clipboard cannot, if defect

--- client/common/source/class/ms123/graphicaleditor/plugins/EditClipBoard.js
+++ client/common/source/class/ms123/graphicaleditor/plugins/EditClipBoard.js
@@ -21,29 +21,28 @@
  * DEALINGS IN THE SOFTWARE.
  **/
 
-qx.Class.define("ms123.graphicaleditor.plugins.EditClipBoard", {
+qx.Class.define( "ms123.graphicaleditor.plugins.EditClipBoard", {
 	extend: qx.core.Object,
 	/******************************************************************************
 	 CONSTRUCTOR
 	 ******************************************************************************/
-	construct: function (namespace) {
-		this.base(arguments);
-		var store = new qx.data.store.Offline("clipboard-"+namespace);
-		var model = null;
-		if (store.getModel() === null) {
-			model = qx.data.marshal.Json.createModel({
-				shapesAsJson : [],
-				namespace : "",
-				useOffset : true
-			}, true);
-			model.setNamespace(namespace);
-			model.setUseOffset(true);
-			model.setShapesAsJson([]);
-			store.setModel(model);
-		} else {
-			model = store.getModel();
+	construct: function( namespace ) {
+		this.base( arguments );
+		var store = null;
+		try {
+			store = new qx.data.store.Offline( "clipboard-" + namespace );
+		} catch ( e ) {
+			var storage = qx.bom.Storage.getLocal();
+			storage.removeItem( "clipboard-" + namespace );
+			store = new qx.data.store.Offline( "clipboard-" + namespace );
 		}
-		this._offlineModel = model;
+		if ( store.getModel() === null ) {
+			var model = this.createModel( namespace );
+			store.setModel( model );
+			this._offlineModel = model;
+		} else {
+			this._offlineModel = store.getModel();
+		}
 	},
 
 
@@ -56,12 +55,12 @@ qx.Class.define("ms123.graphicaleditor.plugins.EditClipBoard", {
 	 STATICS
 	 ******************************************************************************/
 	statics: {
-		__clipboards:{},
-		getClipboard:function(namespace){
-			var clipboard = ms123.graphicaleditor.plugins.EditClipBoard.__clipboards[namespace];
-			if( clipboard == null){
-				clipboard = new ms123.graphicaleditor.plugins.EditClipBoard(namespace);
-				ms123.graphicaleditor.plugins.EditClipBoard.__clipboards[namespace] = clipboard;
+		__clipboards: {},
+		getClipboard: function( namespace ) {
+			var clipboard = ms123.graphicaleditor.plugins.EditClipBoard.__clipboards[ namespace ];
+			if ( clipboard == null ) {
+				clipboard = new ms123.graphicaleditor.plugins.EditClipBoard( namespace );
+				ms123.graphicaleditor.plugins.EditClipBoard.__clipboards[ namespace ] = clipboard;
 			}
 			return clipboard;;
 		}
@@ -70,43 +69,54 @@ qx.Class.define("ms123.graphicaleditor.plugins.EditClipBoard", {
 	 MEMBERS
 	 ******************************************************************************/
 	members: {
-		isOccupied: function () {
+		createModel: function( namespace ) {
+			var model = qx.data.marshal.Json.createModel( {
+				shapesAsJson: [],
+				namespace: "",
+				useOffset: true
+			}, true );
+			model.setNamespace( namespace );
+			model.setUseOffset( true );
+			model.setShapesAsJson( [] );
+			return model;
+		},
+		isOccupied: function() {
 			return this._offlineModel.getShapesAsJson().length > 0;
 		},
-		get:function(){
+		get: function() {
 			var a = this._offlineModel.getShapesAsJson();
-			if( a.getLength ){
-			 return qx.lang.Json.parse(qx.util.Serializer.toJson(a));
-			}else{
+			if ( a.getLength ) {
+				return qx.lang.Json.parse( qx.util.Serializer.toJson( a ) );
+			} else {
 				return a;
 			}
 		},
-		getNamespace:function(){
+		getNamespace: function() {
 			return this._offlineModel.getNamespace();
 		},
-		useOffset:function(){
+		useOffset: function() {
 			return this._offlineModel.getUseOffset();
 		},
-		setUseOffset:function(b){
-			this._offlineModel.setUseOffset(b);
+		setUseOffset: function( b ) {
+			this._offlineModel.setUseOffset( b );
 		},
-		refresh: function (shapes, namespace, useNoOffset) {
-			this._offlineModel.setUseOffset(useNoOffset !== true);
+		refresh: function( shapes, namespace, useNoOffset ) {
+			this._offlineModel.setUseOffset( useNoOffset !== true );
 
-			var json = shapes.map(function (shape) {
+			var json = shapes.map( function( shape ) {
 				var s = shape.toJSON();
 				s.parent = {
 					resourceId: shape.getParentShape().resourceId
 				};
-				s.parentIndex = shape.getParentShape().getChildShapes().indexOf(shape)
+				s.parentIndex = shape.getParentShape().getChildShapes().indexOf( shape )
 				return s;
-			});
-			this._offlineModel.setShapesAsJson(json);
+			} );
+			this._offlineModel.setShapesAsJson( json );
 		}
 	},
 	/******************************************************************************
 	 DESTRUCTOR
 	 ******************************************************************************/
-	destruct: function () {}
+	destruct: function() {}
 
-});
+} );
