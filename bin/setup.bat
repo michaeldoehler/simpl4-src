586bfc0cb844dbb1415b7f776b838f3736efeba2
ms <manfred@ms123.org>
2015-11-11 06:14:07 +0100

    wrong paramter handling

--- bin/setup.bat
+++ bin/setup.bat
@@ -1,5 +1,14 @@
 @echo off
-setx SIMPL4DIR %2
-set SIMPL4DIR=%2
-%2\bin\bash.exe %2\bin\setup.sh %1 %2 %3 %4 %5
+
+set P1=%1
+set P2=%2
+set P3=%3
+set P4=%4
+IF not [%2] == [] GOTO Next1
+set P2=%cd%
+:Next1
+
+setx SIMPL4DIR %P2% >nul
+set SIMPL4DIR=%P2%
+%P2%\bin\bash.exe %P2%\bin\setup.sh %P1% %P2% %P3% %P4%
 
