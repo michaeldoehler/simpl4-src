9424a0fe8c55c882147a0ca25a545935b476cb63
ms <manfred@ms123.org>
2015-11-12 23:52:19 +0100

    jsstrip now local

---
+++ surface/js/bin/jsstrip.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+#########################################################
+# globals
+#########################################################
+OVERWRITE=
+#########################################################
+# functions
+#########################################################
+#########################################################
+# usages
+#########################################################
+usage() {
+   echo "usage: jsstrip.sh [-o] in.js [out.js]"
+}
+#########################################################
+# main
+#########################################################
+shortoptions='o'
+longoptions='overwrite'
+getopt=$(getopt -o $shortoptions --longoptions  $longoptions -- "$@")
+if [ $? != 0 ]; then
+   usage
+   exit 1;
+fi
+
+eval set -- "$getopt"
+while true; do
+   case "$1" in
+      -h|--help)
+         usage
+         exit 1
+      ;;
+      -o|--overwrite)
+         OVERWRITE=true
+         shift 1
+      ;;
+      --)
+         shift 1
+         break
+      ;;
+      
+      *)
+         break
+      ;;
+   esac
+done
+
+INFILE=$1
+java -jar bin/compiler.jar --charset UTF-8 -O WHITESPACE_ONLY $INFILE >/tmp/.js
+#if [ -n "$OVERWRITE" ] ; then
+#   /usr/local/bin/js-beautify   /tmp/.js >$INFILE
+#else
+#   /usr/local/bin/js-beautify   /tmp/.js
+#fi
+cp /tmp/.js $INFILE
