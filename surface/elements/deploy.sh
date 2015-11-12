ec8529ab6fbfe3439694e143927282117122e23f
ms <manfred@ms123.org>
2015-11-12 23:38:06 +0100

    is no longer needed

--- surface/elements/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-make-all.sh
-gzip -f elements.html
-scp elements.html.gz  ms:$simpl4first/client/surface/
-#scp elements.html.gz  ms:$web/client/surface/
--- surface/elements/make-all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-#bower install
-rm -rf bower_components/polymer/  
-find . -name "*.html" -exec sed -i "/polymer.html/d" {} \;
-
-
-vulcanize --config vulcan.config --inline index.html
-mv vulcanized.html elements.html
